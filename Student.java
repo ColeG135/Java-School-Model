5718d3151808dcd41d93a775edc3403befc655f3
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-03-09 14:22:54 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,6 +1,48 @@
 public class Student {
-   Student Tim = new Student("Tim");
-   Section cs = new Section("cs");
-   cs.addStudent("Tim");
-}
+   private Section[] Section;
+   private int grade;
+   private int sectionCount;
+   public int getGrade;
 
+   public Section[] getSection() {
+      return Section;
+   }
+
+   public void setSection(Section[] section) {
+      Section = section;
+   }
+
+   public int getGrade() {
+      return grade;
+   }
+
+   public int getSectionCount() {
+      return sectionCount;
+   }
+
+   public void setSectionCount(int sectionCount) {
+      this.sectionCount = sectionCount;
+   }
+
+   public int getGetGrade() {
+      return getGrade;
+   }
+
+   public void setGetGrade(int getGrade) {
+      this.getGrade = getGrade;
+   }
+
+   public Section getGetSection() {
+      return getSection;
+   }
+
+   public void setGetSection(Section getSection) {
+      this.getSection = getSection;
+   }
+
+   public void setGrade(int g) {
+   }
+   public Section getSection;
+   public void addSectio(Section s){
+   }
+}
