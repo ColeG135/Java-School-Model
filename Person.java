462d9662416cdfcd4eb056ffd5503b7f0afcb16e
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-03-09 14:21:56 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,6 +1,39 @@
 public class Person {
-    School: name;
-    Section; name;
-    Teacher: name, subject;
-    Student: name, grade;
+    private String name;
+    private int id;
+    public String getName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getGetName() {
+        return getName;
+    }
+
+    public void setGetName(String getName) {
+        this.getName = getName;
+    }
+
+    public int getGetId() {
+        return getId;
+    }
+
+    public void setGetId(int getId) {
+        this.getId = getId;
+    }
+
+    public void setname(String n){
+    }
+    public int getId;
+    public void setId(int id){
+    }
 }
