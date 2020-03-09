1255217e445939e3feff387e9d67d99f442971dd
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2020-03-09 14:22:17 -0700

    Update School.java

--- School.java
+++ School.java
@@ -1,6 +1,58 @@
 public class School {
-    public School() {
-        //name:String;
-    }
-}
+    private String name;
+    private Section[] Section;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Section[] getSection() {
+        return Section;
+    }
+
+    public void setSection(Section[] section) {
+        Section = section;
+    }
+
+    public int getSectionCount() {
+        return sectionCount;
+    }
+
+    public void setSectionCount(int sectionCount) {
+        this.sectionCount = sectionCount;
+    }
+
+    public String getGetName() {
+        return getName;
+    }
+
+    public void setGetName(String getName) {
+        this.getName = getName;
+    }
+
+    public int getAddSection() {
+        return addSection;
+    }
+
+    public void setAddSection(int addSection) {
+        this.addSection = addSection;
+    }
+
+    public Section[] getGetSections() {
+        return getSections;
+    }
+
+    public void setGetSections(Section[] getSections) {
+        this.getSections = getSections;
+    }
+
+    private int sectionCount;
+    public String getName;
+
+    int addSection;
+    Section[] getSections;
+}
